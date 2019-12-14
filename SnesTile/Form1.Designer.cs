b1992bd75d7cc695414940e99ef2f438e8d8be86
nambona890 <zander.morrow@gmail.com>
2019-12-14 22:40:14 +1100

    Add project files.

---
+++ SnesTile/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SnesTile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.graphicsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paletteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadGFXDialog = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.snesCanvas = new System.Windows.Forms.PictureBox();
+            this.palDisplay = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.snesCanvas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.palDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.graphicsToolStripMenuItem,
+            this.paletteToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // graphicsToolStripMenuItem
+            // 
+            this.graphicsToolStripMenuItem.Name = "graphicsToolStripMenuItem";
+            this.graphicsToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.graphicsToolStripMenuItem.Text = "Graphics";
+            this.graphicsToolStripMenuItem.Click += new System.EventHandler(this.graphicsToolStripMenuItem_Click);
+            // 
+            // paletteToolStripMenuItem
+            // 
+            this.paletteToolStripMenuItem.Name = "paletteToolStripMenuItem";
+            this.paletteToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.paletteToolStripMenuItem.Text = "Palette";
+            this.paletteToolStripMenuItem.Click += new System.EventHandler(this.paletteToolStripMenuItem_Click);
+            // 
+            // loadGFXDialog
+            // 
+            this.loadGFXDialog.FileName = "loadGFXDialog";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.snesCanvas);
+            this.panel1.Location = new System.Drawing.Point(8, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(406, 384);
+            this.panel1.TabIndex = 0;
+            // 
+            // snesCanvas
+            // 
+            this.snesCanvas.BackColor = System.Drawing.Color.Black;
+            this.snesCanvas.Location = new System.Drawing.Point(0, 0);
+            this.snesCanvas.Name = "snesCanvas";
+            this.snesCanvas.Size = new System.Drawing.Size(384, 384);
+            this.snesCanvas.TabIndex = 2;
+            this.snesCanvas.TabStop = false;
+            this.snesCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.snesCanvas_Paint);
+            // 
+            // palDisplay
+            // 
+            this.palDisplay.BackColor = System.Drawing.Color.Black;
+            this.palDisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.palDisplay.Location = new System.Drawing.Point(8, 424);
+            this.palDisplay.Name = "palDisplay";
+            this.palDisplay.Size = new System.Drawing.Size(260, 260);
+            this.palDisplay.TabIndex = 2;
+            this.palDisplay.TabStop = false;
+            this.palDisplay.Paint += new System.Windows.Forms.PaintEventHandler(this.palDisplay_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 695);
+            this.Controls.Add(this.palDisplay);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SnesTile";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.snesCanvas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.palDisplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem graphicsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paletteToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog loadGFXDialog;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox snesCanvas;
+        private System.Windows.Forms.PictureBox palDisplay;
+    }
+}
+
---
+++ SnesTile/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+
+namespace SnesTile
+{
+    public partial class Form1 : Form
+    {
+        private byte[] tileBuffer = new byte[0x10000];
+        private byte[,] tileBMP = new byte[128,128];
+        private Color[] palette = new Color[256];
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void graphicsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            loadGFXDialog.Filter = "Raw graphics file (*.bin)|*.bin|All files (*.*)|*.*";
+            loadGFXDialog.FilterIndex = 2;
+            loadGFXDialog.Title = "Load Graphics";
+            if (loadGFXDialog.ShowDialog() == DialogResult.OK)
+            {
+                Stream tempFile = loadGFXDialog.OpenFile();
+                byte[] tempBuffer = new byte[1024 * 128];
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    int read;
+                    while((read = tempFile.Read(tempBuffer,0,tempBuffer.Length))>0)
+                    {
+                        ms.Write(tempBuffer, 0, read);
+                    }
+                    tileBuffer = ms.ToArray();
+                }
+                tileBMP = BPL2BMP(tileBuffer);
+                snesCanvas.Invalidate();
+            }
+        }
+
+        private void paletteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            loadGFXDialog.Filter = "Raw palette file (*.mw3)|*.mw3|.PAL file (*.pal)|*.pal|All files (*.*)|*.*";
+            loadGFXDialog.FilterIndex = 3;
+            loadGFXDialog.Title = "Load Palette";
+            if (loadGFXDialog.ShowDialog() == DialogResult.OK)
+            {
+                Stream tempFile = loadGFXDialog.OpenFile();
+                byte[] tempBuffer = new byte[1024];
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    int read;
+                    while ((read = tempFile.Read(tempBuffer, 0, tempBuffer.Length)) > 0)
+                    {
+                        ms.Write(tempBuffer, 0, read);
+                    }
+                    tempBuffer = ms.ToArray();
+                }
+                if(Path.GetExtension(loadGFXDialog.FileName).Contains(".pal"))
+                {
+                    LoadPal(tempBuffer);
+                }
+                else
+                {
+                    LoadMW3(tempBuffer);
+                }
+                palDisplay.Invalidate();
+                snesCanvas.Invalidate();
+            }
+        }
+
+        private void LoadPal(byte[] buffer)
+        {
+            palette = new Color[256];
+            for (int i = 0, j = 0; i < 256; i++)
+            {
+                byte red = buffer[j++];
+                byte green = buffer[j++];
+                byte blue = buffer[j++];
+                palette[i] = Color.FromArgb(red, green, blue);
+            }
+        }
+
+        private void LoadMW3(byte[] buffer)
+        {
+            palette = new Color[256];
+            for (int i = 0, j = 0; i < 256; i++)
+            {
+                ushort tmpColor = Convert.ToUInt16((buffer[j++]) | buffer[j++] << 8);
+                byte red = Convert.ToByte((tmpColor & 0x1F) << 3);
+                byte green = Convert.ToByte((tmpColor & 0x3E0) >> 2);
+                byte blue = Convert.ToByte((tmpColor & 0x7C00) >> 7);
+                palette[i] = Color.FromArgb(red, green, blue);
+            }
+        }
+
+        private byte[,] BPL2BMP(byte[] buffer)
+        {
+            byte[,] indexBMP = new byte[128,128];
+            for(int i=0,j=0;i<Math.Min(buffer.Length,0x2000);)
+            {
+                for(int k=0;k<8;k++)
+                {
+                    byte[] row = new byte[8];
+                    byte bp1 = buffer[i];
+                    byte bp2 = buffer[i+1];
+                    byte bp3 = buffer[i+16];
+                    byte bp4 = buffer[i+17];
+
+                    row[7] = Convert.ToByte((bp1 & 1) | ((bp2 & 1) << 1) | ((bp3 & 1) << 2) | ((bp4 & 1) << 3));
+                    row[6] = Convert.ToByte(((bp1 & 2) >> 1) | ((bp2 & 2)) | ((bp3 & 2) << 1) | ((bp4 & 2) << 2));
+                    row[5] = Convert.ToByte(((bp1 & 4) >> 2) | ((bp2 & 4) >> 1) | ((bp3 & 4)) | ((bp4 & 4) << 1));
+                    row[4] = Convert.ToByte(((bp1 & 8) >> 3) | ((bp2 & 8) >> 2) | ((bp3 & 8) >> 1) | ((bp4 & 8)));
+                    row[3] = Convert.ToByte(((bp1 & 16) >> 4) | ((bp2 & 16) >> 3) | ((bp3 & 16) >> 2) | ((bp4 & 16) >> 1));
+                    row[2] = Convert.ToByte(((bp1 & 32) >> 5) | ((bp2 & 32) >> 4) | ((bp3 & 32) >> 3) | ((bp4 & 32) >> 2));
+                    row[1] = Convert.ToByte(((bp1 & 64) >> 6) | ((bp2 & 64) >> 5) | ((bp3 & 64) >> 4) | ((bp4 & 64) >> 3));
+                    row[0] = Convert.ToByte(((bp1 & 128) >> 7) | ((bp2 & 128) >> 6) | ((bp3 & 128) >> 5) | ((bp4 & 128) >> 4));
+                    i += 2;
+                    int curY = (GetBPLY(i) * 8) + k;
+                    for (int l = 0; l < 8; l++)
+                    {
+                        int curX = (GetBPLX(i) * 8) + l;
+                        indexBMP[curX,curY] = row[l];
+                    }
+                }
+                i += 16;
+            }
+
+            return indexBMP;
+        }
+
+        private int GetBPLY(int loc)
+        {
+            return loc >> 9;
+        }
+
+        private int GetBPLX(int loc)
+        {
+            return (loc >> 5) & 0xf;
+        }
+
+        private Bitmap BMPIndex2RGB(byte[,] indexBMP,Color[] palette)
+        {
+            Bitmap bmp = new Bitmap(128, 128);
+            for(int i=0;i<128;i++)
+            {
+                for(int j=0;j<128;j++)
+                {
+                    bmp.SetPixel(i, j, palette[indexBMP[i, j]]);
+                }
+            }
+            return bmp;
+        }
+
+        private void snesCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            if (tileBMP.Length > 0 && palette.Length > 0)
+            {
+
+                Bitmap canvas = BMPIndex2RGB(tileBMP, palette);
+                e.Graphics.FillRectangle(Brushes.Black, 0, 0, 384, 384);
+                e.Graphics.DrawImage(canvas, 0, 0, 384, 384);
+            }
+        }
+
+        private void palDisplay_Paint(object sender, PaintEventArgs e)
+        {
+            for(int i=0;i<16;i++)
+            {
+                for(int j=0;j<16;j++)
+                {
+                    Brush b = new SolidBrush(palette[(i * 16) + j]);
+                    e.Graphics.FillRectangle(b, j * 16, i * 16, 16, 16);
+                }
+            }
+        }
+    }
+}
